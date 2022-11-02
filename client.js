4e24cbd0f770bf6d2d7c953e733ec7a3477657e8
lhp03 <60330965+lhp03@users.noreply.github.com>
2022-11-02 18:28:40 +0900

    221102

--- client.js
+++ client.js
@@ -1,31 +1,50 @@
-const menuClick = (text) => {
-    console.log(text);
-    let mneu = document.getElementById("menu");
-    let select = menu.Options[menu.selectedIndex].text
+const sendPost = (url) => {
+  const form = document.createElement("form");
+  form.setAttribute("method", "POST");
+  form.setAttribute("action", url);
+  document.characterSet = "utf-8";
+  document.body.appendChild(form);
+  form.submit();
+};
 
-    if(select === "Log an error") {
-        console.log("Log an error");
-    } else if(select === "Log an warning") {
-        console.log("Log an warning");
-    } else if(select === "View errors") {
-        console.log("View errors");
-    } else if(select === "View warnings") {
-        console.log("View warnings");
-    } else if(select === "View all long entires") {
-        console.log("View all entires");
-    }
-}
+const sendGet = (url) => {
+  const form = document.createElement("form");
+  form.setAttribute("method", "GET");
+  form.setAttribute("action", url);
+  document.characterSet = "utf-8";
+  document.body.appendChild(form);
+  form.submit();
+};
+
+const menuClick = async () => {
+  let menu = document.getElementById("menu");
+  let select = menu.options[menu.selectedIndex].value;
+
+  console.log(select);
+
+  if (select === "log_an_error") {
+    sendPost("/log_error");
+  } else if (select === "log_an_warning") {
+    sendPost("/log_warning");
+  } else if (select === "view_errors") {
+    document.location.href = "/errors";
+  } else if (select === "view_warnings") {
+    document.location.href = "/warnings";
+  } else if (select === "view_all") {
+    document.location.href = "/all";
+  }
+};
 
 const logError = async () => {
-    console.log("TEST");
-    try {
-        const response = await fetch("/log_error", {
-            method : "POST"
-        });
-        console.log(response);
-    } catch(err) {
-        console.log(err);
-    }
-}
+  console.log("TEST");
+  try {
+    const response = await fetch("/log_error", {
+      method: "GET",
+    });
+    console.log(response);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 let menu = document.getElementById("menu");
--- server.js
+++ server.js
@@ -1,83 +1,138 @@
 const fs = require("fs");
-const http = require('http');
+const http = require("http");
 
 const err_file_name = "error_log.txt";
 const warn_file_name = "warning_log.txt";
 
 try {
-    if(!fs.existsSync(err_file_name)) {
-        fs.writeFileSync(err_file_name, "");
-    }
-    
-    if(!fs.existsSync(warn_file_name)) {
-        fs.writeFileSync(warn_file_name, "");
-    }
-} catch(err) {
-    console.log(err);
+  if (!fs.existsSync(err_file_name)) {
+    fs.writeFileSync(err_file_name, "");
+  }
+
+  if (!fs.existsSync(warn_file_name)) {
+    fs.writeFileSync(warn_file_name, "");
+  }
+} catch (err) {
+  console.log(err);
 }
 
 const writeErrLog = () => {
-    const date = new Date();
-    const error_log = `Error occurred at: ${date}`;
+  const date = new Date();
+  const error_log = `Error occurred at: ${date}\n`;
 
-    try {
-        fs.appendFileSync(err_file_name, error_log+ "\n");
-    } catch(err) {
-        console.log(err);
-    }
-}
+  try {
+    fs.appendFileSync(err_file_name, error_log);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const writeWarnLog = () => {
-    const date = new Date();
-    const warning_log = `Warning occurred at: ${date}`
+  const date = new Date();
+  const warning_log = `Warning occurred at: ${date}\n`;
 
-    try {
-        fs.appendFileSync(err_file_name, warning_log + "\n");
+  try {
+    fs.appendFileSync(warn_file_name, warning_log);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-    } catch(err) {
-        console.log(err);
-    }
-}
-
-
+const html = fs.readFileSync("index.html", { encoding: "utf-8" });
+const js = fs.readFileSync("client.js", { encoding: "utf-8" });
 
 const server = http.createServer((req, res) => {
-    console.log(req);
-    if(req.method=="GET" && req.url == "/") {
-        const html = fs.readFileSync("index.html", {encoding : "utf-8"});
-        res.writeHead(200, {"Content-type" : "text/html"});
-        res.end(html);
-    } else if(req.method == "GET" && req.url == "/log_error") {
-        writeErrLog();
-        res.writeHead(302, {"Location" : "/"});
-        res.end();
-    } else if(req.method == "POST" && req.url == "/log_warning") {
-        writeWarnLog();
-        res.writeHead(302, {"Location" : "/"});
-        res.end();
-        //todo: save log warning
-    } else if(req.method == "GET" && req.url == "/errors") {
-        res.writeHead(200);
-        try {
-            const errors = fs.readFileSync(err_file_name,{encoding : "utf-8"});
+  if (req.method == "GET" && req.url == "/") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(html);
+  } else if (req.method == "POST" && req.url == "/log_error") {
+    writeErrLog();
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(html);
+    res.write("<h3>An error has been logged</h3>");
+    res.end();
+  } else if (req.method == "POST" && req.url == "/log_warning") {
+    writeWarnLog();
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(html);
+    res.write("<h3>An warning has been logged</h3>");
+    res.end();
+  } else if (req.method == "GET" && req.url == "/errors") {
+    try {
+      const errors = fs
+        .readFileSync(err_file_name, { encoding: "utf-8" })
+        .trim();
 
-        } catch(err) {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(html);
+      res.write(`<div id='error'><h3>Error Log</h3>`);
+      errors.split("\n").map((value) => {
+        res.write(`<p>${value.trim()}</p>`);
+      });
+      res.write(`</div>`);
 
-        }
-        //todo: send error logs
-    } else if(req.method == "GET" && req.url == "/warnings") {
-        res.writeHead(200);
-        //todo: send warning logs
-    } else if(req.method == "GET" && req.url == "/all") {
-        res.writeHead(200);
-        //todo: send error and warning logs
-    } else if(req.method == "GET" && req.url == "/client.js") {
-        const js = fs.readFileSync("client.js", {encoding:"utf-8"});
-        res.writeHead(200, {"Content-type":"text/javascript"});
-        res.end(js);
+      res.end();
+    } catch (err) {
+      console.log(err);
+      res.writeHead(404);
+      res.end();
     }
+  } else if (req.method == "GET" && req.url == "/warnings") {
+    try {
+      const warnings = fs
+        .readFileSync(warn_file_name, { encoding: "utf-8" })
+        .trim();
+
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(html);
+      res.write(`<div id='warning'><h3>Warning Log</h3>`);
+      warnings.split("\n").map((value) => {
+        res.write(`<p>${value.trim()}</p>`);
+      });
+      res.end();
+    } catch (err) {
+      console.log(err);
+      res.writeHead(404);
+      res.end();
+    }
+  } else if (req.method == "GET" && req.url == "/all") {
+    try {
+      const errors = fs
+        .readFileSync(err_file_name, { encoding: "utf-8" })
+        .trim();
+      const warnings = fs
+        .readFileSync(warn_file_name, { encoding: "utf-8" })
+        .trim();
+
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(html);
+      res.write(`<div id='error'><h3>Error Log</h3>`);
+      errors.split("\n").map((value) => {
+        res.write(`<p>${value.trim()}</p>`);
+      });
+      res.write(`</div>`);
+
+      res.write(`<div id='warning'><h3>Warning Log</h3>`);
+      warnings.split("\n").map((value) => {
+        res.write(`<p>${value.trim()}</p>`);
+      });
+      res.write(`</div>`);
+
+      res.end();
+    } catch (err) {
+      console.log(err);
+      res.writeHead(404);
+      res.end();
+    }
+  } else if (req.method == "GET" && req.url == "/client.js") {
+    res.writeHead(200, { "Content-type": "text/javascript" });
+    res.end(js);
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
 });
 
 server.listen(3000, "127.0.0.1", () => {
-    console.log("SERVER LISTNENING...");
-})
+  console.log("SERVER LISTNENING...");
+});
